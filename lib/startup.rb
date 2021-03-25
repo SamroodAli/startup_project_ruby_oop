f24bf9811a0a183f69165d29175e4fa9d78ac355
samroodAli <devsamrood@gmail.com>
2021-03-25 13:24:10 +0530

    class Statup initialize function

--- lib/startup.rb
+++ lib/startup.rb
@@ -1,5 +1,12 @@
 require "employee"
 
 class Startup
+  def initialize(name, funding,salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employee = []
+  end
+
 
 end
